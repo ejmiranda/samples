55e6b2ce555d5c6720ab2ea81a68134527e0aea3
Edgar Miranda <edgar.j.miranda@me.com>
2018-09-27 12:56:09 -0500

    Add Palindromes

---
+++ palindromes/README.md
@@ -0,0 +1,6 @@
+## Palindromes
+
+**Description:**
+- From The Odin Project's [Curriculum](https://www.theodinproject.com/courses/web-development-101/lessons/fundamentals-part-5).
+- Check if a string is a palindrome and return `true` or `false`.
+- Work with single words, multiple words and punctuation.
---
+++ palindromes/palindromes.js
@@ -0,0 +1,14 @@
+const palindromes = function(str) {
+  let strArray = [];
+  for (let char of str.toLowerCase().split(``)) {
+    if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122)
+      strArray.push(char);
+  }
+  for (let i = 0; i < Math.floor(strArray.length / 2); i++) {
+    if (strArray[i] !== strArray[strArray.length - 1 - i])
+      return false;
+  }  
+  return true;
+}
+
+module.exports = palindromes
