a1c7d617b8d1ecc0cad89c43e8953352bb6a9c67
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-11-13 01:12:45 -0500

    Add Caesar Cipher (Ruby) using the algorithm from the JS version

---
+++ caesar-cipher-ruby/README.md
@@ -0,0 +1,7 @@
+## Caesar Cipher (Ruby)
+
+- From The Odin Project's [Curriculum](https://www.theodinproject.com/lessons/ruby-caesar-cipher).
+- Use the Caesar Cipher on a string, given a shift value.
+- Replace letters in the string with gsub.
+- Retain capitalization and punctuation. 
+- Work with positive and negative shift values.
---
+++ caesar-cipher-ruby/main.rb
@@ -0,0 +1,11 @@
+def caesar_cipher (string, key)
+  string.gsub(/[a-zA-Z]/) do |char|
+    base = char.ord < 97 ? 65 : 97 #Uppercase (65-90) or lowercase (97-122)
+    shift_code = char.ord + key
+    adjusted_code = shift_code - 26 * ((shift_code - base) / 26).floor
+    adjusted_code.chr
+  end
+end
+puts ("What a string!") # Original
+puts ("Bmfy f xywnsl!") # Expected
+puts caesar_cipher("What a string!", 5)
