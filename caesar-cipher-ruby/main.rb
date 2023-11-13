3e80b23917aea6be0c86a5d910390bf9330cb423
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-11-13 06:56:00 -0500

    Update main.rb with minor formatting

--- caesar-cipher-ruby/main.rb
+++ caesar-cipher-ruby/main.rb
@@ -1,6 +1,6 @@
 def caesar_cipher (string, key)
   string.gsub(/[a-zA-Z]/) do |char|
-    base = char.ord < 97 ? 65 : 97 #Uppercase (65-90) or lowercase (97-122)
+    base = char.ord < 97 ? 65 : 97 # Uppercase (65-90) or lowercase (97-122)
     shift_code = char.ord + key
     adjusted_code = shift_code - 26 * ((shift_code - base) / 26).floor
     adjusted_code.chr
