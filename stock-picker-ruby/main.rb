bb0c3d8c2d8b042ce9e7fe9baa000b314eb49a47
Edgar Miranda <19721204+ejmiranda@users.noreply.github.com>
2023-11-14 14:31:34 +0000

    Refactor without hash

--- stock-picker-ruby/main.rb
+++ stock-picker-ruby/main.rb
@@ -1,16 +1,17 @@
 def stock_picker(prices)
-  picker = { d_buy: 0, d_sell: 0, profit: 0}
+  days = []
+  profit = 0
   prices.each_with_index do |price, day|
     next_prices = prices.slice(day + 1, prices.length)
     next_prices.each_with_index do |next_price, next_day|
-      if next_price - price > picker[:profit]
-        picker[:profit] = next_price - price
-        picker[:d_buy] = day
-        picker[:d_sell] = day + next_day + 1
+      if next_price - price > profit
+        profit = next_price - price
+        days = [] << day << day + next_day + 1 # Because next_day always starts with 0.
       end
     end
   end
-  [picker[:d_buy], picker[:d_sell]]
+  days
 end
 
-p stock_picker([17,3,6,9,15,8,6,1,10])
+puts "#{stock_picker([17,3,6,9,15,8,6,1,10])} <- Result"
+puts '[1, 4] <- Expected'
